ab3d580076d66b058812e4edd32a78bdb6ebe702
thucuc01 <70868459+thucuc01@users.noreply.github.com>
2021-01-18 09:34:02 +0700

    bt1

---
+++ src/main/java/controller/MyController.java
@@ -0,0 +1,12 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MyController {
+    @RequestMapping("/test")
+    public String test(){
+        return "index";
+    }
+}
